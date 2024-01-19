132592704322beaf1724fcc238c32233dea64f30
wangunyup <wangunyuP@LeadForensics.com>
2024-01-19 16:17:50 +0000

    Add repository functionality and refactor tests

--- backend/src/repository/todo.ts
+++ backend/src/repository/todo.ts
@@ -1,13 +1,43 @@
+export class TodoRepository {
 
-let todoList = {
-  todos: [
-    {
-      "id": 1,
-      "task": "This is a todo example"
+  todoList = {
+    todos: [
+      {
+        'id': 1,
+        'task': 'This is a todo example'
+      }
+    ]
+  };
+
+  getTodos() {
+    return this.todoList;
+  };
+
+  addTodo(item) {
+    if (item !== '') {
+      this.todoList.todos.push(item);
+      return this.todoList;
+    } else {
+      throw new Error('You must type in a todo');
     }
-  ]
-};
+  }
 
-module.exports = {
-  getTodos: () => Promise.resolve(todoList)
-};
+  deleteTodos(id) {
+    if (this.todoList.todos === id) {
+      this.todoList.todos = this.todoList.todos.filter(todo => todo.id !== id);
+      return this.todoList;
+    } else {
+      throw new Error(`Todo with ID ${id} cannot be deleted.`);
+    }
+  }
+
+  updateTodos(id, item) {
+    const index = this.todoList.todos.findIndex(todo => todo.id === id);
+    if (index !== -1) {
+      this.todoList.todos[index] = {...this.todoList.todos[index], ...item};
+    } else {
+      throw new Error(`Todo with ID ${id} does not exist.`);
+    }
+    return this.todoList;
+  }
+}
--- backend/test/repository/todo.test.ts
+++ backend/test/repository/todo.test.ts
@@ -1,98 +1,79 @@
-const repository = require('../../src/repository/todo');
+import * as todoRepository from '../../src/repository/todo';
 
-jest.mock('../../src/repository/todo');
+jest.mock('../../src/repository/todo', () => ({
+  TodoRepository: jest.fn().mockImplementation(() => ({
+    getTodos: jest.fn(),
+    addTodo: jest.fn(),
+    deleteTodos: jest.fn(),
+    updateTodos: jest.fn(),
+  })),
+}));
 
 describe('TODO repository', () => {
+  let repository: jest.Mocked<todoRepository.TodoRepository>;
+
   beforeEach(() => {
     jest.clearAllMocks();
+    repository = new todoRepository.TodoRepository() as jest.Mocked<todoRepository.TodoRepository>;
   });
 
   it('should return the todo list', async () => {
-    repository.getTodos.mockResolvedValue({
-      todos: [
-        {
-          id: 1,
-          task: "This is a todo example",
-        },
-      ],
-    });
-
     const expected = {
       todos: [
         {
           id: 1,
-          task: "This is a todo example",
+          task: 'This is a todo example',
         },
       ],
     };
+    (repository.getTodos as jest.Mock).mockResolvedValue(expected);
 
-    const actual = await repository.getTodos();
+    const actual = repository.getTodos();
     expect(actual).toEqual(expected);
   });
 
   it('should add to the todo list', async () => {
-    repository.addTodo.mockResolvedValue({
-      todos: [
-        {
-          id: 1,
-          task: "This is a todo example",
-        },
-        {
-          id: 2,
-          task: "This is another todo example",
-        },
-      ],
-    });
-
-    const item = {
-      id: 2,
-      task: "This is another todo example",
-    };
-
     const expected = {
       todos: [
         {
           id: 1,
-          task: "This is a todo example",
+          task: 'This is a todo example',
         },
         {
           id: 2,
-          task: "This is another todo example",
+          task: 'This is another todo example',
         },
       ],
     };
 
-    const actual = await repository.addTodo(item);
+    const item = {
+      id: 2,
+      task: 'This is another todo example',
+    };
+
+    (repository.addTodo as jest.Mock).mockResolvedValue(expected);
+
+    const actual = repository.addTodo(item);
     expect(actual).toEqual(expected);
   });
 
   it('should delete from the todo list', async () => {
-    repository.deleteTodos.mockResolvedValue({
-      todos: [],
-    });
-
     const id = 1;
 
     const expected = {
       todos: [],
     };
 
-    const actual = await repository.deleteTodos(id);
+    (repository.deleteTodos as jest.Mock).mockResolvedValue(expected);
+
+    const actual = repository.deleteTodos(id);
     expect(actual).toEqual(expected);
   });
 
-  it('should persist changes to the todo list', () => {
-    repository.updateTodos.mockReturnValue({
-      todos: [
-        {
-          id: 1,
-          task: "This is an updated todo",
-        },
-      ],
-    });
+  it('should persist changes to the todo list', async () => {
 
     const item = {
-      task: "This is an updated todo",
+      task: 'This is an updated todo',
     };
     const id = 1;
 
@@ -100,51 +81,59 @@ describe('TODO repository', () => {
       todos: [
         {
           id: 1,
-          task: "This is an updated todo",
+          task: 'This is an updated todo',
         },
       ],
     };
+
+    (repository.updateTodos as jest.Mock).mockResolvedValue(expected);
 
     const actual = repository.updateTodos(id, item);
     expect(actual).toEqual(expected);
   });
 
-  it('should not persist changes to a non existent element in the todo list', () => {
-    repository.updateTodos.mockImplementation(() => {
+  it('should not persist changes to a non existent element in the todo list', async () => {
+
+
+    const item = {
+      task: 'This is a non existent todo',
+    };
+    const id = 2;
+
+    (repository.updateTodos as jest.Mock).mockImplementation(() => {
       throw new Error(`Todo with ID ${id} does not exist.`);
     });
 
-    const item = {
-      task: "This is a non existent todo",
-    };
-    const id = 2;
-
-    expect(() => repository.updateTodos(id, item)).toThrow('Todo with ID 2 does not exist.');
+    await expect(async () => {
+      repository.updateTodos(id, item);
+    }).rejects.toThrow('Todo with ID 2 does not exist.');
   });
 
-  it('should not delete a non existent element in the todo list', () => {
-    repository.deleteTodos.mockImplementation(() => {
+  it('should not delete a non existent element in the todo list', async () => {
+    const id = 2;
+
+
+    (repository.deleteTodos as jest.Mock).mockImplementation(() => {
       throw new Error(`Todo with ID ${id} cannot be deleted.`);
     });
 
-    const item = {
-      task: "This is a non existent todo",
-    };
-    const id = 2;
-
-    expect(() => repository.deleteTodos(id, item)).toThrow('Todo with ID 2 cannot be deleted.');
+    await expect(async () => {
+      repository.deleteTodos(id);
+    }).rejects.toThrow((`Todo with ID 2 cannot be deleted.`));
   });
 
-  it('should not add an empty task in the todo list', () => {
-    repository.addTodo.mockImplementation(() => {
-      throw new Error(`You must type in a todo`);
+  it('should not add an empty task in the todo list', async () => {
+    const item = {
+      id: 2,
+      task: '',
+    };
+
+    (repository.addTodo as jest.Mock).mockImplementation(() => {
+      throw new Error('You must type in a todo');
     });
 
-    const item = {
-      task: "",
-    };
-    const id = 2;
-
-    expect(() => repository.addTodo(id, item)).toThrow(`You must type in a todo`);
+    await expect(async () => {
+      repository.addTodo(item);
+    }).rejects.toThrow('You must type in a todo');
   });
 });
